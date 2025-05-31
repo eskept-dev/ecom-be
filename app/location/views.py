ddcd908849ad434824f7ed0be0ff0cec44ff9aa3
Ryan <nguyenduylan711@gmail.com>
2025-05-31 10:15:46 +0700

    implement search

--- app/location/views.py
+++ app/location/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
 from app.base.pagination import CustomPagination
 from app.location import serializers
@@ -13,6 +14,21 @@ class LocationModelViewSet(ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = serializers.LocationSerializer
     pagination_class = CustomPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(code__icontains=search_query) |
+                Q(address__icontains=search_query) |
+                Q(province__icontains=search_query) |
+                Q(city__icontains=search_query) |
+                Q(district__icontains=search_query) |
+                Q(ward__icontains=search_query)
+            )
+        return queryset
 
     def get_permissions(self):
         """
