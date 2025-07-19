d27b8642c4b824891048d05850107b27784327b8
Ryan <nguyenduylan711@gmail.com>
2025-07-19 11:41:40 +0700

    add scheduled task

--- app/core/celery.py
+++ app/core/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 from app.core import settings
 
@@ -13,8 +14,19 @@ celery_app = Celery(
     accept_content=settings.CELERY_ACCEPT_CONTENT,
     task_serializer=settings.CELERY_TASK_SERIALIZER,
     result_serializer=settings.CELERY_RESULT_SERIALIZER,
+    broker_connection_retry_on_startup=True,
+    result_backend_transport_options={
+        'retry_on_startup': True,
+    },
     timezone=settings.CELERY_TIMEZONE,
 )
 
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'precompute_product_price_task': {
+        'task': 'precompute_product_price_task',
+        'schedule': crontab(minute='*/30')
+    },
+}
